5ec2d2a6f58015c534f389f2ecb8b46510f8ded8
shweta <shwatakarvanjikar@gmail.com>
2022-11-18 20:05:10 +0530

    first commit

---
+++ ArrayList/src/com/ArrayList/shweta/CreateArrayList.java
@@ -0,0 +1,17 @@
+package com.ArrayList.shweta;
+
+import java.util.ArrayList;
+
+public class CreateArrayList {
+
+	public static void main(String[] args) {
+		ArrayList<Integer>al=new ArrayList<>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		
+		System.out.println("arraylist element-" +al);
+
+	}
+
+}
